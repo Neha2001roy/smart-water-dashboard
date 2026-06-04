5c5d2a900764c5bd8babf44cb5109a56c4949e20
Neha2001roy <neha2001roy@gmail.com>
2026-06-04 13:08:40 +0530

    app.py

--- app.py
+++ app.py
@@ -69,17 +69,22 @@ st.markdown("""
 st.markdown("---")
 
 # =====================================================
+# =====================================================
 # LOAD DATA
 # =====================================================
 
 df = pd.read_excel(
     "Data/Smart_Water_Treatment_Dummy_Data.xlsx",
-    
+    sheet_name=0
 )
-df.columns = df.columns.str.strip()
 
-st.write("Columns:", df.columns.tolist())
-st.write(df.head())
+df.columns = df.columns.str.strip().str.lower()
+
+st.write("Columns Found:")
+st.write(df.columns.tolist())
+
+st.write("Sample Data:")
+st.dataframe(df.head())
 # =====================================================
 # SIDEBAR
 # =====================================================
